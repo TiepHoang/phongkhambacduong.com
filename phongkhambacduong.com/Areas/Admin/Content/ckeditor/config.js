2b31de399c1e880406216479595f4478253e3137
boolmain <hoangtiep96na@gmail.com>
2017-04-22 17:34:38 +0700

    up code

---
+++ phongkhambacduong.com/Areas/Admin/Content/ckeditor/config.js
@@ -0,0 +1,21 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+
+    config.enterMode = CKEDITOR.ENTER_BR;
+    config.toolbar = 'Full';
+    config.filebrowserBrowseUrl = '/Areas/Admin/Content/ckfinder/ckfinder.html';
+    config.filebrowserImageBrowseUrl = '/Areas/Admin/Content/ckfinder/ckfinder.html?type=Images';
+    config.filebrowserFlashBrowseUrl = '/Areas/Admin/Content/ckfinder/ckfinder.html?type=Flash';
+    config.filebrowserUploadUrl = '/Areas/Admin/Content/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Files';
+    config.filebrowserImageUploadUrl = '/Areas/Admin/Content/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Images';
+    config.filebrowserFlashUploadUrl = '/Areas/Admin/Content/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash';
+    config.filebrowserWindowWidth = '1000';
+    config.filebrowserWindowHeight = '700';
+};
---
+++ phongkhambacduong.com/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(phongkhambacduong.com.Startup))]
+namespace phongkhambacduong.com
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
